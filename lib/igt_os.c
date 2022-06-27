9b130b0fc030e1a2abee52298374ea2c4684ae0a
Jake Freeland <jakesfreeland@gmail.com>
2022-06-26 22:02:58 -0500

    added hardware querying capability for FreeBSD in igt_os.c

--- lib/igt_os.c
+++ lib/igt_os.c
@@ -44,6 +44,9 @@
 #include <sys/sysinfo.h>
 #elif defined(HAVE_SWAPCTL) /* Solaris */
 #include <sys/swap.h>
+#elif defined(__FreeBSD__)
+#include <sys/sysctl.h>
+#include <sys/types.h>
 #endif
 #include <sys/resource.h>
 
@@ -148,10 +151,17 @@ igt_get_avail_ram_mb(void)
 	npages = sysconf(_SC_AVPHYS_PAGES);
 
 	retval = (uint64_t) pagesize * npages;
+#elif defined(__FreeBSD__) /* FreeBSD */
+	uint64_t npages, pagesize;
+	size_t npages_len = sizeof(npages);
+	size_t pagesize_len = sizeof(pagesize);
+
+	sysctlbyname("vm.stats.vm.v_free_count", &npages, &npages_len, NULL, 0);
+	sysctlbyname("vm.stats.vm.v_page_size", &pagesize, &pagesize_len, NULL, 0);
+
+	retval = pagesize * npages;
 #else
-/* TODO: FreeBSD - GET AVAIL RAM */
-/* #error "Unknown how to get available RAM for this OS" */
-#warning "Unknown how to get available RAM for this OS"
+#error "Unknown how to get available RAM for this OS"
 #endif
 
 	return retval / (1024*1024);
@@ -212,8 +222,14 @@ igt_get_total_swap_mb(void)
 	free(buf);
 
 	retval = (uint64_t) pagesize * totalpages;
+#elif defined(__FreeBSD__) /* FreeBSD */
+	uint64_t swap_total;
+	size_t swap_total_len = sizeof(swap_total);
+
+	sysctlbyname("vm.swap_total", &swap_total, &swap_total_len, NULL, 0);
+
+	retval = swap_total;
 #else
-/* TODO: FreeBSD - GET SWAP SPACE */
 #warning "Unknown how to get swap size for this OS"
 	return 0;
 #endif
